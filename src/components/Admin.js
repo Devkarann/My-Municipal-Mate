fdc0fb6dcc94d68fcf138a6fe6f5462baa820be0
chetanpatil06 <chetan06042001@gmail.com>
2024-08-12 13:31:52 +0530

    Admin Login UI done

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle, FaFacebook } from "react-icons/fa";
+import {useNavigate} from 'react-router-dom'
 import AuthService from '../service/MyMunicipalService'
 import './SignPage.css';
 
 
-const SignInPage = ({ onLogin }) => {
+const Admin = ({ onLogin }) => {
   const [usernameOrEmail, setUsernameOrEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     if (usernameOrEmail && password) {
       try {
         const response = await AuthService.signIn(usernameOrEmail, password);
+        if (response.data && response.data.success) {
+            navigate('/dashadmin');
+            onLogin();
+        } else {
+            // Handle login failure (e.g., incorrect credentials)
+            setErrorMessage(response.data.message || "Login failed");
+        }
         console.log(response);
         onLogin();
       } catch (error) {
@@ -27,7 +36,7 @@ const SignInPage = ({ onLogin }) => {
     <div className="sign-in-page-container">
     <div className="login-wrapper">
       <div className="login-container">
-        <h2>Login</h2>
+        <h2>Admin Login</h2>
         {errorMessage && <p className="error-message">{errorMessage}</p>}
         <input
           type="text"
@@ -42,18 +51,6 @@ const SignInPage = ({ onLogin }) => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <button onClick={handleLogin}>Login</button>
-        <div className="login-links">
-          <Link to="/register">Register</Link>
-          <Link to="/forgot-password">Forgot Password?</Link>
-        </div>
-        <div className="oauth-buttons">
-          <button className="oauth-button google" disabled style={{cursor: 'default'}}>
-            <FaGoogle className="icon" /> Google
-          </button>
-          <button className="oauth-button facebook" disabled style={{cursor: 'default'}}>
-            <FaFacebook className="icon" /> Facebook
-          </button>
-        </div>
       </div>
     </div>
     </div>
@@ -61,4 +58,4 @@ const SignInPage = ({ onLogin }) => {
   );
 };
 
-export default SignInPage;
+export default Admin;
